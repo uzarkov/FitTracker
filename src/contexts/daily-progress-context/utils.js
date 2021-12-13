ec6fa6283c92bd92d32be9de6fec476b3772962e
Witold Brzozowski <45402204+boavenn@users.noreply.github.com>
2021-12-13 17:48:34 +0100

    FT-5 Add product context (#10)

--- src/contexts/daily-progress-context/utils.js
+++ src/contexts/daily-progress-context/utils.js
@@ -11,3 +11,15 @@ export const getDailyProgressPlaceholder = (date, caloricDemand) => {
         isPlaceholder: true,
     }
 }
+
+export const countTotalsOf = (dailyProgress) => {
+    const productsKcal = dailyProgress.products.reduce((acc, v) => acc + v.kcal, 0)
+    const burnedKcal = dailyProgress.activities.reduce((acc, v) => acc + v.burnedKcal, 0)
+
+    return {
+        totalKcal: productsKcal - burnedKcal,
+        totalCarbs: dailyProgress.products.reduce((acc, v) => acc + v.carbs, 0),
+        totalProteins: dailyProgress.products.reduce((acc, v) => acc + v.proteins, 0),
+        totalFats: dailyProgress.products.reduce((acc, v) => acc + v.fats, 0),
+    }
+}
--- src/views/DashboardView.jsx
+++ src/views/DashboardView.jsx
@@ -1,13 +1,17 @@
 import moment from 'moment';
 import React, { useEffect } from 'react';
 import { Dimensions, Pressable, ScrollView, StyleSheet, Text, View } from "react-native";
+import { useState } from 'react/cjs/react.development';
 import { TopBar } from '../components/TopBar';
 import { addActivity, removeActivity } from '../contexts/activity-context/actions';
 import { signOut } from '../contexts/auth-context/actions/signOut';
 import { useAuth } from '../contexts/auth-context/authContext';
 import { fetchDailyProgress } from '../contexts/daily-progress-context/actions/fetchDailyProgress';
 import { useDailyProgress } from '../contexts/daily-progress-context/dailyProgressContext';
-import { getDailyProgressPlaceholder } from '../contexts/daily-progress-context/utils';
+import { countTotalsOf, getDailyProgressPlaceholder } from '../contexts/daily-progress-context/utils';
+import { fetchCurrentGoal } from '../contexts/goal-context/actions/fetchCurrentGoal';
+import { useGoal } from '../contexts/goal-context/goalContext';
+import { addProduct, removeProduct } from '../contexts/product-context/actions';
 
 export const DashboardView = () => {
     const [authState, authDispatch] = useAuth();
@@ -19,16 +23,52 @@ export const DashboardView = () => {
     const today = moment().format('DD-MM-YYYY');
     const dailyProgress = days[today] || getDailyProgressPlaceholder(today, 0);
 
+    const [goalState, goalDispatch] = useGoal();
+    const { activeGoal, fetching: fetchingGoal } = goalState;
+
+    const [activeGoalFetched, setActiveGoalFetched] = useState(false);
+
     useEffect(() => {
-        fetchDailyProgress(dailyProgressDispatch, {
-            uid: user.uid,
-            date: today,
-            defaultCaloricDemand: 0, // TODO: Use caloric demand from active goal here
-        });
+        fetchCurrentGoal(goalDispatch, {
+            uid: user.uid
+        })
+        setActiveGoalFetched(true)
     }, [])
+
+    useEffect(() => {
+        if (activeGoalFetched && !fetchingGoal) {
+            fetchDailyProgress(dailyProgressDispatch, {
+                uid: user.uid,
+                date: today,
+                defaultCaloricDemand: activeGoal.caloricDemand || 0,
+            });
+        }
+    }, [activeGoalFetched, fetchingGoal])
 
     const onSignOut = () => {
         signOut(authDispatch)
+    }
+
+    const addDailyProgressProduct = () => {
+        addProduct(dailyProgressDispatch, {
+            uid: user.uid,
+            dailyProgress,
+            newProduct: {
+                name: "White rice",
+                kcal: 357,
+                proteins: 7.9,
+                carbs: 79,
+                fats: 0.9
+            }
+        })
+    }
+
+    const removeDailyProgressProduct = (index) => {
+        removeProduct(dailyProgressDispatch, {
+            uid: user.uid,
+            dailyProgress,
+            index,
+        })
     }
 
     const addDailyProgressActivity = () => {
@@ -50,6 +90,8 @@ export const DashboardView = () => {
         })
     }
 
+    const dailyProgressTotals = countTotalsOf(dailyProgress)
+
     return (
         <>
             <TopBar />
@@ -57,10 +99,10 @@ export const DashboardView = () => {
                 <ScrollView contentContainerStyle={styles.container} style={styles.scrollView}>
                     <Text style={styles.text}>{`Date: ${dailyProgress.date}`}</Text>
                     <Text style={styles.text}>{`Target kcal: ${dailyProgress.targetKcal}`}</Text>
-                    <Text style={styles.text}>{`Total kcal: ${dailyProgress.totalKcal}`}</Text>
-                    <Text style={styles.text}>{`Total carbs: ${dailyProgress.totalCarbs}`}</Text>
-                    <Text style={styles.text}>{`Total proteins: ${dailyProgress.totalProteins}`}</Text>
-                    <Text style={styles.text}>{`Total fats: ${dailyProgress.totalFats}`}</Text>
+                    <Text style={styles.text}>{`Total kcal: ${dailyProgressTotals.totalKcal}`}</Text>
+                    <Text style={styles.text}>{`Total carbs: ${dailyProgressTotals.totalCarbs}`}</Text>
+                    <Text style={styles.text}>{`Total proteins: ${dailyProgressTotals.totalProteins}`}</Text>
+                    <Text style={styles.text}>{`Total fats: ${dailyProgressTotals.totalFats}`}</Text>
                     {dailyProgress.activities.map((activity, idx) => (
                         <>
                             <Text style={styles.text}>{`Name: ${activity.name}`}</Text>
@@ -75,15 +117,36 @@ export const DashboardView = () => {
                     ))}
                     <Pressable
                         style={styles.button}
-                        onPress={onSignOut}
+                        onPress={addDailyProgressActivity}
                     >
-                        <Text style={styles.text}>Sign out</Text>
+                        <Text style={styles.text}>Add activity</Text>
+                    </Pressable>
+                    {dailyProgress.products.map((product, idx) => (
+                        <>
+                            <Text style={styles.text}>{`Name: ${product.name}`}</Text>
+                            <Text style={styles.text}>{`Kcal: ${product.kcal}`}</Text>
+                            <Text style={styles.text}>{`Proteins: ${product.proteins}`}</Text>
+                            <Text style={styles.text}>{`Carbs: ${product.carbs}`}</Text>
+                            <Text style={styles.text}>{`Fats: ${product.fats}`}</Text>
+                            <Pressable
+                                style={styles.button}
+                                onPress={() => removeDailyProgressProduct(idx)}
+                            >
+                                <Text style={styles.text}>Remove</Text>
+                            </Pressable>
+                        </>
+                    ))}
+                    <Pressable
+                        style={styles.button}
+                        onPress={addDailyProgressProduct}
+                    >
+                        <Text style={styles.text}>Add product</Text>
                     </Pressable>
                     <Pressable
                         style={styles.button}
-                        onPress={addDailyProgressActivity}
+                        onPress={onSignOut}
                     >
-                        <Text style={styles.text}>Add some activity</Text>
+                        <Text style={styles.text}>Sign out</Text>
                     </Pressable>
                 </ScrollView>
             </View>
@@ -111,7 +174,7 @@ const styles = StyleSheet.create({
         display: 'flex',
         justifyContent: 'center',
         alignItems: 'center',
-        width: '30%',
+        width: '50%',
         minWidth: 100,
         borderWidth: 1,
         borderColor: 'white',
